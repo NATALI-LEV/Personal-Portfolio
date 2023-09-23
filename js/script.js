24a1d64aa67782f772cd9b991c88ad7196cc0f90
NATALI-LEV <natalillev@gmail.com>
2023-09-23 10:51:31 +0300

    added typing animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* === typing animation ===*/
+var typed = new Typed(".typing",{
+    strings:["Computer Science student", "backend developer" , "frontend developer", "Gamer","fullstack developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
