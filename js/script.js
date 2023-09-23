09927f7204f84945cc395a2c9f4a88b6b9f90b60
NATALI-LEV <natalillev@gmail.com>
2023-09-23 15:25:48 +0300

    added section movement

--- js/script.js
+++ js/script.js
@@ -16,8 +16,16 @@ const nav = document.querySelector(".nav"),
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function()
         {
+            for(let i=0 ; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("back-section");
+        }
             for(let j=0 ; j<totalNavList;j++)
             {
+                if(navList[j].querySelector("a").classList.contains("active"))
+                {
+                    allSection[j].classList.add("back-section");
+                }
                 navList[j].querySelector("a").classList.remove("active");
             }
             this.classList.add("active")
