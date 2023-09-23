128b057630f5702df4bf856ef4100f7240cdf7a5
NATALI-LEV <natalillev@gmail.com>
2023-09-23 12:33:26 +0300

    deleted wrong files

--- font-awesome-4.7.0/script.js
+++
@@ -1,7 +0,0 @@
-/* === typing animation ===*/
-var typed = new Typed(".typing",{
-    strings:["Computer Science student", "backend developer" , "frontend developer", "Gamer"],
-    typeSpeed:100,
-    backSpeed:60,
-    loop:true
-})
